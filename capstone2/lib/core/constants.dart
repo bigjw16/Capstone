4e739d663be0d0595007bcd506cf7c5ebbc6cc18
bigjw16 <woogood14@gmail.com>
2026-05-31 01:35:37 +0900

    1.0.2

--- capstone2/lib/core/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color chickYellow = Color(0xFFFFE978);
-const Color chickOrange = Color(0xFFF4A646);
-const Color chickBrown = Color(0xFF3B2A12);
-const Color chickRed = Color(0xFFE24A2A);
